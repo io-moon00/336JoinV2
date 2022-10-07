058ddffddc05a612ecd87d9392a02c13310b83e4
FiratYildirim85 <firat.yildirim2904@gmail.com>
2022-10-07 17:19:53 +0200

    test2

--- scrips/addTask.js
+++ scrips/addTask.js
@@ -1,4 +1,21 @@
+let selectedUsers = [];
+
+
 async function init(){
     await includeHTML();
     markActivePage('addTask');
 }
+
+function setTask() {
+    task = {
+        title: form.elements["tasktitle"].value,
+       // urgency: getUrgency(),     Test
+        category: form.elements["category"].value,
+        duedate: form.elements["dueDate"].value,
+        description: form.elements["desc"].value,
+        assignedTo: selectedUsers,
+       // status: "toBoard",
+       // creator: currentUserId,
+    };
+}
+
