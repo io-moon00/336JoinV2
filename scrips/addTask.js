91b3b73387886d7b76f5f43418c647516c378831
FiratYildirim85 <firat.yildirim2904@gmail.com>
2022-10-07 21:37:07 +0200

    addTaskJS

--- scrips/addTask.js
+++ scrips/addTask.js
@@ -1,4 +1,4 @@
-let selectedUsers = [];
+let tasks = [];
 
 
 async function init(){
@@ -6,16 +6,93 @@ async function init(){
     markActivePage('addTask');
 }
 
-function setTask() {
-    task = {
-        title: form.elements["tasktitle"].value,
-       // urgency: getUrgency(),     Test
-        category: form.elements["category"].value,
-        duedate: form.elements["dueDate"].value,
-        description: form.elements["desc"].value,
-        assignedTo: selectedUsers,
-       // status: "toBoard",
-       // creator: currentUserId,
-    };
+
+function getTitle () {
+   let title = document.getElementById('tasktitle').value;
+   return title; 
 }
+
+
+/*function getContacts () {
+    let contacts = document.getElementById('contacts').value;
+    return contacts;
+ }
+*/
+
+
+function getDueDate () {
+    let dueDate = document.getElementById('dueDate').value;
+    return dueDate;
+ }
+
+
+function getCategorie () {
+    let category = document.getElementById('category').value;
+    return category;
+ }
+
+
+ function getPriority () {
+    let priority = document.getElementById('priority').value;
+    return priority;
+ }
+
+
+ function getDescription () {
+    let description = document.getElementById('desc').value;
+    return description;
+ }
+
+ function setID () {
+    let index = tasks.length -1;
+    let id = tasks[index].id +1;
+    return id;
+ }
+
+ function addTask () {
+    let task = {
+        id: setID(),
+        category: getCategorie(),
+        title: getTitle(),
+        description: getDescription(),
+        status: 'toDo',
+        priority: getPriority (),
+        dueDate: getDueDate(),
+    }
+    
+    tasks.push(task);
+    clearTaskForm ();
+    window.open('../pages/board.html');
+}
+
+async function getTasks (){
+    let url = '/tasks.json';
+    try {
+        let response = await fetch(url);
+        tasks = await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+ }
+
+
+function clearTaskForm () {
+    document.getElementById('category').value = '';
+    document.getElementById('title').innerHTML = '';
+    document.getElementById('description').innerHTML = '';
+    document.getElementById('priority').value = '';
+    document.getElementById('dueDate').value = '';
+    document.getElementById('contact').value = '';
+}
+
+
+
+
+
+ 
+
+
+
+
+
 
