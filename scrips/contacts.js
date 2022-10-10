6ed5e909ba813526f9fd47c417f86e0b5593ec2f
Buelent <ekibuli@hotmail.dem>
2022-10-10 19:37:20 +0200

    contact change

--- scrips/contacts.js
+++ scrips/contacts.js
@@ -7,109 +7,88 @@ let contacts = [{
     'name': 'Anton Mayer',
     'shortName' : 'AM',
     'email' : 'anton@gmail.com',
-    'mobil' : '+49 1111 111 11 11'
+    'mobil' : '+49 1111 111 11 11',
+    'color' : '#FF7A00'
 },
 {
     'name': 'Anja Schulz',
     'shortName' : 'AS',
     'email' : 'schulz@hotmail.com',
-    'mobil' : '+49 1111 111 11 11'
+    'mobil' : '+49 1111 111 11 11',
+    'color' : '#9327FF'
 },
 {
     'name': 'Benedikt Ziegler',
     'shortName' : 'BZ',
     'email' : 'Benedikt@gmail.com',
-    'mobil' : '+49 1111 111 11 11'
+    'mobil' : '+49 1111 111 11 11',
+    'color' : '#29ABE2'
 },
 {
     'name': 'David Eisenberg',
     'shortName' : 'DE',
     'email' : 'davidberg@gmail.com',
-    'mobil' : '+49 1111 111 11 11'
+    'mobil' : '+49 1111 111 11 11',
+    'color' : '#FC71FF'
 },
 {
     'name': 'Eva Fischer',
     'shortName' : 'DE',
     'email' : 'eva@gmail.com',
-    'mobil' : '+49 1111 111 11 11'
+    'mobil' : '+49 1111 111 11 11',
+    'color' : '#02CF2F'
 },
 {
     'name': 'Emmanuel Mauer',
     'shortName' : 'EM',
     'email' : 'emmanuelMa@gmail.com',
-    'mobil' : '+49 1111 111 11 11'
+    'mobil' : '+49 1111 111 11 11',
+    'color' : '#AF1616'
 },
 {
     'name': 'Marcel Bauer',
     'shortName' : 'MB',
     'email' : 'bauer@gmail.com',
-    'mobil' : '+49 1111 111 11 11'
+    'mobil' : '+49 1111 111 11 11',
+    'color' : '#462F8A'
 },
 {
     'name': 'Nina Molberg',
     'shortName' : 'NM',
     'email' : 'molberg@gmx.com',
-    'mobil' : '+49 1111 111 11 11'
+    'mobil' : '+49 1111 111 11 11',
+    'color' : '#FF4646'
 },
 {
     'name': 'Olaf Schuhmacher',
     'shortName' : 'OS',
     'email' : 'olaf.schuhmacher@gmx.com',
-    'mobil' : '+49 1111 111 11 11'
+    'mobil' : '+49 1111 111 11 11',
+    'color' : '#FF7A00'
 },
 ];
-
-let colors = {
-    A:'#FF7A00', 
-    B:'#9327FF', 
-    C:'#29ABE2', 
-    D:'#FC71FF', 
-    E:'#02CF2F', 
-    F:'#AF1616', 
-    G:'#462F8A', 
-    H:'#FF4646',
-    I:'',
-    J:'',
-    K:'',
-    L:'',
-    M:'',
-    N:'',
-    O:'',
-    P:'',
-    Q:'',
-    R:'',
-    S:'',
-    T:'',
-    V:'',
-    W:'',
-    X:'',
-    Y:'',
-    Z:'',
-};
-
+    
 function renderallcontact() {
-    let postContactContainer = dokument.getElementById('postContactContainer');
-    postContactContainer.innerHTML = ``;
 
     for (let i = 0; i < contacts.length; i++) {
-        postContactContainer.innerHTML += generalCotactPost(i);
+        let letter = getFirstCharacter(i);
+        console.log(letter);
+        document.getElementById(letter).innerHTML += generalCotactPost(i);
+
     }
 };
-function renderColors() {
-    for (let j = 0; j < colors.length; j++) {
-    }
-    generalCotactPost(i);
+
+// 
+function getFirstCharacter(i) {
+let letter = contacts[i].name.charAt(0);
+return letter;
 }
 
 function generalCotactPost(i) {
     const contact = contacts[i];
-    const color = colors[j];
     return`
-    <div>
-        <div class="headline">A</div>
-    </div>
     <div class="contact-name-container display-start" onclick="showContact()" onclick="returnShowContact()" >
-        <span class="kontakt-circle" style="${color['A']}">${contact['shortName']}</span>
+        <span class="kontakt-circle" style="backgroundcolor: ${contact['color']};">${contact['shortName']}</span>
         <div class="mail-name">
             <span class="contact-name">${contact['name']}</span>
             <span class="contact-name" style="color:#4589FF">${contact['email']}</span>
@@ -117,11 +96,18 @@ function generalCotactPost(i) {
     </div>
     `
 }
- 
 
+let clicked = false;
 function showContact() {
-    document.getElementById('contact').classList.add('show-overlay-contact');
-    document.getElementById('contact').classList.remove('overlay-contact');
+
+    if (clicked) {
+        returnShowContact();
+        clicked = false;   
+    }else {
+        document.getElementById('contact').classList.add('show-overlay-contact');
+        document.getElementById('contact').classList.remove('overlay-contact');
+        clicked = true;
+    }
 }
 
 function returnShowContact() {
