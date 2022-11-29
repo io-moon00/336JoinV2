3e2f6aff801e8d2437ea6e4162cd2b7d0d5fc44f
Buelent <ekibuli@hotmail.dem>
2022-11-29 23:22:53 +0100

    contacts change

--- scrips/contacts.js
+++ scrips/contacts.js
@@ -1,5 +1,8 @@
-setURL('https://gruppe-336.developerakademie.net/smallest_backend_ever');
 let contacts = [];//backend geladen werden
+let currentUser = [];
+
+setURL('https://gruppe-336.developerakademie.net/smallest_backend_ever');
+
 
 let colors = ['#FF7A00', '#9327FF', '#29ABE2', '#FC71FF', '#02CF2F', '#AF1616', '#462F8A', '#FF4646', 'orange', 'deeppink'];
 function save() {
@@ -23,6 +26,7 @@ async function renderAllContact() {
         showEditContact(i);
         detailContact(i);
         newContact(i);
+        getRandomColor(i);
     }
 }
 function getFirstCharacter(i) {
@@ -34,7 +38,7 @@ function getFirstCharacter(i) {
 function generalContactPost(i) {
     return`
     <div class="contact-name-container display-start" onclick="showContact(${i})">
-        <span class="kontakt-circle" style="background-color:${colors[i]};">${contacts[i].shortName}</span>
+        <span class="kontakt-circle" style="background-color:${colors[i]};">${contacts[i]['shortName']}</span>
         <div class="mail-name">
             <span class="contact-name">${contacts[i]['name']}</span>
             <span class="contact-name" style="color:#4589FF">${contacts[i]['email']}</span>
@@ -66,41 +70,62 @@ function returnShowContact() {
     document.getElementById('contact').classList.add('overlay-contact');
 }
 
-function addNewContact() {
-    let firstName = document.getElementById('contactfirstName');
-    let secondName = document.getElementById('contactsecondName');
-    let email = document.getElementById('contactEmail');
-    let mobile = document.getElementById('contactPhone');
+function setContactID() {
+    let index = contacts.length -1;
+    let id = contacts[index].id +1;
+    return id;
+}
+
+function shortNames() {
+    contacts = contacts.sort((a, b) => {
+        if (a.name < b.name) {
+            return -1;
+        }
+    });
+}
+
+function getRandomColor() {
+    for (let i = 0; i < colors.length; i++) {
+        colors += colors[Math.floor(Math.random() * 16)];
+    }
+    return colors;
+}
+
+function addNewContact(event) {
+    event.preventDefault();
+    let name = document.getElementById('contactName').value;
+    let email = document.getElementById('contactEmail').value;
+    let mobile = document.getElementById('contactPhone').value;
+    let id = contacts.length;
+    let shortName = shortNames();
+    let color = getRandomColor();
 
     let contactInfo = {
-        "firstName": firstName.value,
-        "secondName": secondName.value,
-        "email": email.value,
-        "phone": mobile.value,
-        "shortName": (firstName.value.charAt(0) + secondName.value.charAt(0)).toUpperCase(),
+        "name": name,
+        "email": email,
+        "phone": mobile,
+        "shortName": shortName,
+        "color": color,
+        "id": id,
         };
 
         contacts.push(contactInfo);
+        saveContactsToServer();
 
-        save();
+        name = '';
+        email = '';
+        mobile = '';
+ 
+        renderAllContact();
 
-        firstName.value = '';
-        secondName.value = '';
-        email.value = '';
-        mobile.value = '';
+}
 
-        generalContactPost();
-
-
-
-
-    //backend.setItem('contacts', JSON.stringify(contacts));
-    // clear contacact Form
-    // generate Msg
+async function saveContactsToServer() {
+    await backend.getItem('contacts', JSON.stringify(contacts));
 }
 
 // Name für Küzel aufteilen//
-/*function getshortName() {
+function getshortName() {
     let names = string.split(contacts[i].name);
     let shortName = contacts[i]['shortName'];
     names = names[0].substring(0, 1).toUpperCase();
@@ -109,7 +134,12 @@ function addNewContact() {
         shortName += names[names.length - 1].substring(0, 1).toUpperCase();
     }
     return shortName;
-}*/
+}
+
+function removeContact(i) {
+    contacts.splice(i, 1);
+    generalContactPost()
+}
 
 function getColorForName(shortName) {
     let number = (shortName.charCodeAt(0) + shortName.charCodeAt(1)) % colors.length;
@@ -134,27 +164,27 @@ function newContact(i) {
 function returnNewContact() {
     document.getElementById('newContact').classList.add('d-none');
 }
-function cancelList() {
-    document.getElementById('contactName').value = '';
-    document.getElementById('contactEmail').value = '';
-    document.getElementById('contactPhone').value = '';
-}
 
 function addTaskContact() {
     window.open('./addTask.html');
 }
 
 function detailContact(i) {
-    document. getElementById('contact').innerHTML +=`
-    <div class="display-column" id="detailContact">
+    document. getElementById('contact').innerHTML +=
+    ` <div class="display-column" id="detailContact">
         <div class="display-center margin-top">
             <div class="kontakt-circle-big" style="background-color: ${colors[i]};>
             <span class="contact-name-contact">${contacts[i]['shortName']}</span>
             </div>
-            <div class="mail-name">
-                <div class="contact-name" style="color:#4589FF">
-                    <img src="../img/plus-blue.svg">
-                    <span onclick="addTaskContact()" class="add-task">Add Task</span>
+            <div>
+                <div class="contact-name-h2">
+                    <span><b>${contacts[i]['name']}</b></span>
+                </div>
+                <div class="mail-name">
+                    <div class="contact-name" style="color:#4589FF">
+                        <img src="../img/plus-blue.svg">
+                        <span onclick="addTaskContact()" class="add-task">Add Task</span>
+                    </div>
                 </div>
             </div>
             <div class="crossEditContact"><img onclick="showContact()" src="../img/close-cross.svg"></div>
@@ -171,17 +201,16 @@ function detailContact(i) {
                     <span><b>Email</b></span>
                 </div>
                 <div>
-                    <span" style="color:#4589FF">${contacts[i]['email']}</span>
+                    <span style="color:#4589FF">${contacts[i]['email']}</span>
                 </div>
                 <div class="margin-top">
                     <span><b>Mobil:</b></span>
                 </div>
                 <div>
-                    <span>${contacts[i]['phone']}</span>
+                    <span>${contacts[i]['mobil']}</span>
                 </div>
         </div>
-    `
-    ;
+    `;
 }
 
 function editContact(i) {
@@ -215,17 +244,17 @@ function showEditContact(i) {
         </div>
     </div>
     <form class="new-contact-list">
-        <div class="kontakt-circle-add" style="background-color:${colors[i]};">${contacts[i]}</div>
+        <div class="kontakt-circle-add" style="background-color:${colors[i]};">${contacts[i].shortName.id}</div>
         <div class="new-contact-list-colum">
-            <input id="editContactName" pattern="[A-Za-z]+" required minlength="2" class="input-title-user" type="text" placeholder="Name">
+            <input id="editContactName" pattern="[A-Za-z]+" required minlength="2" class="input-title-user" type="text" placeholder="${contacts[i].name}">
             <img src="../img/User.svg">
         </div>
         <div class="new-contact-list-colum">
-            <input id="editContactEmail" required class="input-title-user" type="email" placeholder="Email">
+            <input id="editContactEmail" required class="input-title-user" type="email" placeholder="${contacts[i].email}">
             <img src="../img/eMail-icon.svg">
         </div>
         <div class="new-contact-list-colum">
-            <input id="editContactPhone" class="input-title-user" type="number" min="10" placeholder="Phone">
+            <input id="editContactPhone" class="input-title-user" type="number" min="10" placeholder="${contacts[i].mobil}">
             <img src="../img/phone.svg">
         </div>
         <div class="btn-conect-btn">
